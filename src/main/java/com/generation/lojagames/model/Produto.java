4b6611a13487099fd96e6d65b6b6bb81a757c9a2
Gabriel Camargo <camargoxg@gmail.com>
2024-07-10 14:45:06 -0300

    ::sparkles: feat: criação da classes produto e categoria e relacionamento

---
+++ src/main/java/com/generation/lojagames/model/Produto.java
@@ -0,0 +1,93 @@
+package com.generation.lojagames.model;
+
+import org.springframework.format.annotation.NumberFormat;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O atributo nome é obrigatório!")
+	@Size(min = 5, max = 255, message = "O atributo nome deve conter no mínimo 5 e no máximo 255 caracteres.")
+	private String nome;
+	
+	@NotBlank(message = "O atributo descrição é obrigatório!")
+	@Size(min = 10, max = 1000, message = "O atributo descrição deve conter no m´nimo 10 e no máximo 1000 caracteres.")
+	private String descricao;
+	
+	@NotBlank(message = "O atributo preço é obrigatório!")
+	@NumberFormat(pattern = "#.###.##")
+	private float preco;
+	
+	@NotBlank(message = "O atributo quantidade é obrigatório!")
+	private int quantidade;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public float getPreco() {
+		return preco;
+	}
+
+	public void setPreco(float preco) {
+		this.preco = preco;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	
+	
+}
